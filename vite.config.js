67694e38793808a32405660e7504ca8d9df40a0c
Mr. Blue <94145567+the-blue-lord@users.noreply.github.com>
2025-07-19 03:57:33 +0200

    Reconfigure building
    Externalizes local single-game build configuration to .env file for better configuration clarity.
    
    Adds dotenv node dependencies to import data from the .env file
    
    Fixes inconsistent indentation in vite config file.

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,43 @@
 import { defineConfig } from "vite";
 import fs from "fs";
 import path from "path";
+import dotenv from "dotenv";
 
-const pagesDir = path.resolve(__dirname, "public");
+dotenv.config();
 
-function findHtmlFiles(dir, input = {}) {
-  const files = fs.readdirSync(dir);
-  for (const file of files) {
-    const fullPath = path.join(dir, file);
-    const stat = fs.statSync(fullPath);
-    if (stat.isDirectory()) {
-      findHtmlFiles(fullPath, input);
-    } else if (file.endsWith(".html")) {
-      const name = path.relative(pagesDir, fullPath).replace(/\\/g, "/").replace(/\.html$/, "");
-      input[name] = fullPath;
+function findHtmlFiles(pages_dir, dir, input = {}) {
+    const files = fs.readdirSync(dir);
+    for (const file of files) {
+        const fullPath = path.join(dir, file);
+        const stat = fs.statSync(fullPath);
+        
+        if (stat.isDirectory()) {
+            findHtmlFiles(pages_dir, fullPath, input);
+        }
+        else if (file.endsWith(".html")) {
+            const name = path.relative(pages_dir, fullPath).replace(/\\/g, "/").replace(/\.html$/, "");
+            input[name] = fullPath;
+        }
     }
-  }
-  return input;
+    return input;
 }
 
-export default defineConfig({
-  root: "public",
-  build: {
-    outDir: "../build",
-    rollupOptions: {
-      input: findHtmlFiles(pagesDir)
-    }
-  }
-});
+export default defineConfig(getConfigElement());
+
+function getConfigElement() {
+    const BUILD_GAME = process.env.BUILD_GAME == "true";
+    const GAME_NAME = process.env.GAME_NAME;
+    const GAME_VERSION = process.env.GAME_VERSION;
+
+    const root_folder =  BUILD_GAME ? `public/games/${GAME_NAME}` : "public"
+    const pagesDir = path.resolve(__dirname, root_folder);
+    return {
+        root: root_folder,
+        build: {
+            outDir: BUILD_GAME ? `../bluebooth-${GAME_NAME}-${GAME_VERSION}/build` : "../build",
+            rollupOptions: {
+                input: findHtmlFiles(pagesDir, pagesDir)
+            }
+        }
+    };
+}
