785f03bcc54fd526dff97d8a7493ef06830ae22f
Mr. Blue <94145567+the-blue-lord@users.noreply.github.com>
2025-12-26 00:16:30 +0100

    Snake 3.0.0-aplha.1 [Lobby interface]
    Adds player kicking feature, cleans the user interface and improves communication between the client and the server.

--- public/games/snake/lobby/lobby.js
+++ public/games/snake/lobby/lobby.js
@@ -13,19 +13,16 @@ socket.on("connect", () => {
     if(room_id && usrnm) socket.emit("join", room_id, usrnm, (data) => {
         if(data.status != "ok") return window.location.replace(`/games/snake/join?room=${room_id}`);
         if(socket.id == data.host) {
+            document.querySelector(":root").style.setProperty("--remove-button-display", "default");
             const btn = document.getElementById("start-lobby");
 
-            btn.onclick = () => {
-                socket.emit(
-                    "start-game-req",
-                    room_id,
-                    () => window.location.replace(`/games/snake/game?room=${room_id}`)
-                );
-            }
+            btn.onclick = () => socket.emit("start-game-req", room_id);
 
             btn.disabled = false;
             btn.style.display = "block";
         }
+
+        console.log(data);
         
         for(const index in data.ids) {
             const player_div = getPlayerDiv(data.ids[index], data.players[index], data.ids[index] == data.host);
@@ -41,15 +38,51 @@ function getPlayerDiv(id, name, host = false) {
     const player_div = document.createElement("div");
     player_div.innerHTML = `${host ? "HOST " : ""}${name} (${id})`;
     player_div.id = id;
+    player_div.classList.add("player-div");
+    if(!host) {
+        const button_div = document.createElement("button");
+        button_div.id = "kick-player";
+        button_div.classList.add("remove-button")
+        button_div.innerHTML = "X";
+        button_div.onclick = () => {
+            socket.emit("kick-player", id, room_id);
+        }
+        player_div.appendChild(button_div);
+    }
     return player_div;
 }
 
-socket.on("new-user", (username, id) => {
+function repairPlayerData(data_recovery) {
+    console.log(data_recovery);
+    const server_list = new Map(data_recovery.players.map(obj => [obj.id, obj]));
+    const client_list = new Map(
+        Array.from(
+            document.getElementById("player-list").querySelectorAll(".player-div")
+        ).map(div => [div.id, div])
+    );
+
+    console.log(server_list, client_list);
+
+    for(const [id, div] of client_list) if(!server_list.has(id)) div.remove();
+    for(const [id, data] of server_list) {
+        if(client_list.has(id)) continue;
+        const player_div = getPlayerDiv(data.id, data.name);
+        player_list.appendChild(player_div);
+    }
+}
+
+socket.on("new-user", (username, id, data_recovery) => {
     const player_div = getPlayerDiv(id, username);
     player_list.appendChild(player_div);
+    repairPlayerData(data_recovery);
 });
 
-socket.on("user-left", (id) => {
+socket.on("user-left", (id, data_recovery) => {
     const player_div = document.getElementById(id);
     player_div.remove();
+    repairPlayerData(data_recovery);
+});
+
+socket.on("kicked", () => {
+    window.location.href="/games/snake";
 });
